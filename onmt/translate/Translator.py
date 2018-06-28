83162fba76f60947732249ab6057e17411f7b8a7
TS-Liu <1347481725@qq.com>
2018-06-28 20:56:34 +0800

    Default

--- onmt/translate/Translator.py
+++ onmt/translate/Translator.py
@@ -149,7 +149,7 @@ class Translator(object):
             # in the decoder
             vocab = self.fields["tgt"].vocab
             tokens=[]
-            for tok in list(zip(inp[0])):
+            for tok in inp[0].numpy():
                     tokens.append(vocab.itos[tok])
 
             inp = inp.unsqueeze(2)
