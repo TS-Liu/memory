8a4c6b38f0cac07b5cd02542bf07b4d8f1a47a0c
TS-Liu <1347481725@qq.com>
2018-10-22 16:56:20 +0800

    Default

--- onmt/io/TextDataset.py
+++ onmt/io/TextDataset.py
@@ -484,8 +484,6 @@ class Field(torchtext.data.Field):
                 self.init_token, self.eos_token).count(None) - 2
         padded, lengths = [], []
         for x in minibatch:
-            if not self.use_vocab:
-                x= float(x)
             if self.pad_first:
                 padded.append(
                     [pad_token] * max(0, max_len - len(x)) +
