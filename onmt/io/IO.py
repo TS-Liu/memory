2b261c77e2baf7f857b54ff9af9311a9507db1de
TS-Liu <1347481725@qq.com>
2018-11-02 20:39:57 +0800

    def

--- onmt/io/IO.py
+++ onmt/io/IO.py
@@ -130,7 +130,7 @@ def make_features(batch, side, data_type='text'):
         A sequence of src/tgt tensors with optional feature tensors
         of size (len x batch).
     """
-    assert side in ['src', 'tgt', 'tgt_m', 'tgt_m_p']
+    assert side in ['src', 'tgt', 'src_m', 'tgt_m', 'tgt_m_p']
     if isinstance(batch.__dict__[side], tuple):
         data = batch.__dict__[side][0]
     else:
